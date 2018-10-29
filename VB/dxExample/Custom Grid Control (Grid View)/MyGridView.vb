1842dd01b1f786447530440c596234956eff5a0d
DevExpressExampleBot <fakeEmail>
2018-10-29 19:05:56 +0300

    Source auto update [skip ci]

---
+++ VB/dxExample/Custom Grid Control (Grid View)/MyGridView.vb
@@ -0,0 +1,29 @@
+﻿Imports System
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Data.Helpers
+
+Namespace MyXtraGrid
+    Public Class MyGridView
+        Inherits DevExpress.XtraGrid.Views.Grid.GridView
+
+        Public Sub New()
+            Me.New(Nothing)
+        End Sub
+        Public Sub New(ByVal grid As DevExpress.XtraGrid.GridControl)
+            MyBase.New(grid)
+        End Sub
+        Protected Overrides ReadOnly Property ViewName() As String
+            Get
+                Return "MyGridView"
+            End Get
+        End Property
+        Protected Overrides Function ConvertGridFilterToDataFilter(ByVal criteria As CriteriaOperator, Optional ByVal forceFindFilter As Boolean = False) As CriteriaOperator
+            If Not String.IsNullOrEmpty(FindFilterText) Then
+                Dim lastParserResults As FindSearchParserResults = (New FindSearchParser()).Parse(FindFilterText, GetFindToColumnsCollection())
+                Dim findCriteria As CriteriaOperator = CustomCriteriaHelper.Create(lastParserResults, FilterCondition.Contains, IsServerMode)
+                Return criteria And findCriteria
+            End If
+            Return criteria
+        End Function
+    End Class
+End Namespace
